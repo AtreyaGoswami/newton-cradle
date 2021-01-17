6e6a04f70e6b014cdd250935bec4cbbf20df1fc4
AtreyaGoswami <72393490+AtreyaGoswami@users.noreply.github.com>
2021-01-17 21:18:19 +0530

    Update roofclass.js

--- roofclass.js
+++ roofclass.js
@@ -1,4 +1,29 @@
-class box {
-var roof = createsprite(400,350);
-var roof.shapeColor = color("yellow");
+class roof{
+	constructor(x,y,w,h){
+		var options = {
+			isStatic:true			
+			}
+		this.x=x;
+		this.y=y;
+    this.h=h
+		this.w=w
+		this.body=Bodies.rectangle(x, y, w, h , options);
+ 		World.add(world, this.body);
+
+	}
+	display()
+	{
+			
+			var ground=this.body.position;		
+
+			push()
+			translate(groundPos.x, groundPos.y);
+			rectMode(CENTER)
+			//strokeWeight(4);
+			fill(128,128,128)
+			rect(0,0,this.w, this.h);
+			pop()
+			
+	}
+
 }
